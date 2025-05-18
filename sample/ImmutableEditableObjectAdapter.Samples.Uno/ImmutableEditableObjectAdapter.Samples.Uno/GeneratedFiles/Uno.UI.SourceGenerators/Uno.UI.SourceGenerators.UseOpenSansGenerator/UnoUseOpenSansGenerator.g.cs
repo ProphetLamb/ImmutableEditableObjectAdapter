7c8a47efde35f97c51a279f815de996e93d97e22
ProphetLamb <prophet.lamb@gmail.com>
2025-05-18 10:54:37 +0200

    Update generated files

--- sample/ImmutableEditableObjectAdapter.Samples.Uno/ImmutableEditableObjectAdapter.Samples.Uno/GeneratedFiles/Uno.UI.SourceGenerators/Uno.UI.SourceGenerators.UseOpenSansGenerator/UnoUseOpenSansGenerator.g.cs
+++
@@ -1,8 +0,0 @@
-﻿internal static class __UnoUseOpenSansInitializer
-{
-	[global::System.Runtime.CompilerServices.ModuleInitializerAttribute]
-	internal static void Initialize()
-	{
-		global::Uno.UI.FeatureConfiguration.Font.DefaultTextFontFamily = "ms-appx:///Uno.Fonts.OpenSans/Fonts/OpenSans.ttf";
-	}
-}
